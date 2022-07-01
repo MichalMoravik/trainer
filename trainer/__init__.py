053431b38c55aaa7749a4f19b4398c2237acbad3
MichalMoravik <michal.moravik@icloud.com>
2022-07-01 21:50:18 +0200

    fixing interval

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -3,22 +3,25 @@ import time
 
 class Trainer:
     def __init__(self):
-      self.start = 0
+      self.last_start = 0
+      self.last_end = 0
       self.first_start = 0
       self.current_measurement_name = None
       self.metrics = {}
 
     def __enter__(self):
-        self.start = time.time()
+        self.last_start = time.time()
 
         if self.first_start == 0:
-            self.first_start = self.start
+            self.first_start = self.last_start
 
     def __exit__(self, exc_type, exc_value, traceback):
+        self.last_end = time.time()
+
         self.metrics[self.current_measurement_name] = {
-            'start': self.start,
-            'end': time.time(),
-            'interval': time.time() - self.start
+            'start': self.last_start,
+            'end': self.last_end,
+            'interval': self.last_end - self.last_start
         }
 
     def __call__(self, name):
@@ -26,10 +29,10 @@ class Trainer:
         return self
 
     def add_total(self, name='total_execution'):
-        end = time.time()
+        self.last_end = time.time()
 
         self.metrics[name] = {
                 'start': self.first_start,
-                'end': end,
-                'interval': end - self.first_start
+                'end': self.last_end,
+                'interval': self.last_end - self.first_start
         }
