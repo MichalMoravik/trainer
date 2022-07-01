ec752d35077f8e998b5f33b1c8f18f793a1e327d
MichalMoravik <michal.moravik@icloud.com>
2022-07-01 21:11:27 +0200

    added total exe time

--- trainer/__init__.py
+++ trainer/__init__.py
@@ -4,11 +4,15 @@ import time
 class Trainer:
     def __init__(self):
       self.start = 0
+      self.first_start = 0
       self.current_measurement_name = None
       self.metrics = {}
 
     def __enter__(self):
         self.start = time.time()
+
+        if self.first_start == 0:
+            self.first_start = self.start
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.metrics[self.current_measurement_name] = {
@@ -20,3 +24,12 @@ class Trainer:
     def __call__(self, name):
         self.current_measurement_name = name
         return self
+
+    def add_total(self, name='total_execution'):
+        end = time.time()
+
+        self.metrics[name] = {
+                'start': self.first_start,
+                'end': end,
+                'interval': end - self.first_start
+        }
--- trainer/tests/trainer.py
+++ trainer/tests/trainer.py
@@ -6,10 +6,18 @@ from trainer import Trainer
 class TrainerTest(unittest.TestCase):
     def test_adding_to_metrics(self):
         trainer = Trainer()
+
         with trainer('r'):
             pass
 
-        self.assertIsNotNone(trainer.metrics['r'])
+        self.assertIsNotNone(trainer.metrics.get('r'))
+
+    def test_add_total(self):
+        trainer = Trainer()
+
+        trainer.add_total()
+
+        self.assertIsNotNone(trainer.metrics.get('total_execution'))
 
 
 if __name__ == '__main__':
