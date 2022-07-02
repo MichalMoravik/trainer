5416b0c607709a39d7a904d362766f976f735bfd
MichalMoravik <michal.moravik@icloud.com>
2022-07-02 22:52:35 +0200

    added test for appends twice

--- trainer/tests/trainer.py
+++ trainer/tests/trainer.py
@@ -11,28 +11,26 @@ class TrainerTest(unittest.TestCase):
     def setUp(self):
         self.trainer = Trainer()
 
-    def test_append_metric(self):
-        start = 1656752928.3975601
-        end = 1656752955.581867
-        expected = {'r': {
-            'start': start,
-            'end': end,
-            'interval': end - start
-        }}
-
-        self.trainer._append_metric('r', start, end)
-
-        self.assertEqual(self.trainer.metrics, expected)
-
     def test_ctx_appends(self):
         with self.trainer('t'):
             pass
+
+        self.assertIsNotNone(self.trainer.metrics.get('t'))
+
+    def test_ctx_appends_twice(self):
+        with self.trainer('t'):
+            pass
+        with self.trainer('m'):
+            pass
+
+        self.assertIsNotNone(self.trainer.metrics.get('m'))
         self.assertIsNotNone(self.trainer.metrics.get('t'))
 
     def test_ctx_without_name_fail(self):
         with self.assertRaises(Exception):
             with self.trainer:
                 pass
+
         with self.assertRaises(Exception):
             with self.trainer(''):
                 pass
@@ -92,6 +90,19 @@ class TrainerTest(unittest.TestCase):
 
         for v in m['total_execution'].values():
             self.assertTrue(type(v) is int)
+
+    def test_append_metric(self):
+        start = 1656752928.3975601
+        end = 1656752955.581867
+        expected = {'r': {
+            'start': start,
+            'end': end,
+            'interval': end - start
+        }}
+
+        self.trainer._append_metric('r', start, end)
+
+        self.assertEqual(self.trainer.metrics, expected)
 
 
 if __name__ == '__main__':
