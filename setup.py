fc42de7b07afbf55687aebb3f3e2dfc7854be63d
MichalMoravik <michal.moravik@icloud.com>
2022-07-03 00:25:18 +0200

    added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+      name='trainer',
+      version='0.1.0',
+      description='Lightweight context-based library for benchmarking',
+      packages=['trainer']
+)
+
