f9199847922ef86d564cb61e001b0c415105901b
MichalMoravik <michal.moravik@icloud.com>
2022-07-02 11:22:34 +0200

    updated tests and added start measuring

--- trainer/tests/trainer.py
+++ trainer/tests/trainer.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 
 from trainer import Trainer
 
@@ -7,13 +8,30 @@ class TrainerTest(unittest.TestCase):
     def setUp(self):
         self.trainer = Trainer()
 
-    def test_adding_to_metrics(self):
-        with self.trainer('r'):
+    def test_append_metric(self):
+        start = 1656752928.3975601
+        end = 1656752955.581867
+        expected = {'r': {
+            'start': start,
+            'end': end,
+            'interval': end - start
+        }}
+
+        self.trainer._append_metric('r', start, end)
+
+        self.assertEqual(self.trainer.metrics, expected)
+
+    def test_add_total_when_metric(self):
+        with self.trainer('n'):
             pass
 
-        self.assertIsNotNone(self.trainer.metrics.get('r'))
+        self.trainer.add_total()
 
-    def test_add_total(self):
+        self.assertIsNotNone(self.trainer.metrics.get('total_execution'))
+
+    def test_add_total_when_started(self):
+        self.trainer.start_measuring()
+
         self.trainer.add_total()
 
         self.assertIsNotNone(self.trainer.metrics.get('total_execution'))
