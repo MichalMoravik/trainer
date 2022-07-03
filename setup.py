254f5740db0569c33ccdae97068f91fc622f68ff
MichalMoravik <michal.moravik@icloud.com>
2022-07-03 16:27:30 +0200

    updated package name for Pypi

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 
 setup(
-      name='trainer',
+      name='trainer-py',
       version='0.1.0',
       description='Lightweight context-based library for benchmarking',
       packages=['trainer']
