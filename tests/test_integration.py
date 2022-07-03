df0b73b51850d271570c4b5b4c2db7549ccdbdee
MichalMoravik <michal.moravik@icloud.com>
2022-07-04 01:04:18 +0200

    finished switching tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -6,12 +6,19 @@ from trainer import Trainer, DummyTrainer
 class IntegrationTests(unittest.TestCase):
     def test_switching_to_dummy(self):
         CONFIG_VAR = False
-
         trainer = Trainer() if CONFIG_VAR else DummyTrainer()
 
-        m = trainer.round(4).start_measuring().add_total().metrics
+        m = trainer.round(4).start_measuring().add_total('t').metrics
 
-        self.assertDictEqual(m, {})
+        self.assertFalse(m)
+
+    def test_switching_to_trainer(self):
+        CONFIG_VAR = True
+        trainer = Trainer() if CONFIG_VAR else DummyTrainer()
+
+        m = trainer.round(2).start_measuring().add_total().metrics
+
+        self.assertTrue(m)
 
 
 if __name__ == '__main__':
